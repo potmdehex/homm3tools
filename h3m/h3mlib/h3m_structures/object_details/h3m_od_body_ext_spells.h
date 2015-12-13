00245479f92b63a1cf4a80c012956fd73cd9aa25
potmdehex <potmdehex@h3minternals.net>
2015-12-14 01:38:48 +0900

    add bitfield for spells for use in od

---
+++ h3m/h3mlib/h3m_structures/object_details/h3m_od_body_ext_spells.h
@@ -0,0 +1,86 @@
+// Created by John Åkerblom 2015-12-14
+
+#ifndef __H3M_OD_BODY_EXT_SPELLS_H_DEF__
+#define __H3M_OD_BODY_EXT_SPELLS_H_DEF__
+
+#pragma pack(push, 1)
+
+struct H3M_OD_BODY_EXT_SPELLS
+{
+    unsigned int summon_boat : 1;
+    unsigned int scuttle_boat : 1;
+    unsigned int visions : 1;
+    unsigned int view_earth : 1;
+    unsigned int disguise : 1;
+    unsigned int view_air : 1;
+    unsigned int fly : 1;
+    unsigned int water_walk : 1;
+    unsigned int dimension_door : 1;
+    unsigned int town_portal : 1;
+    unsigned int quicksand : 1;
+    unsigned int land_mine : 1;
+    unsigned int force_field : 1;
+    unsigned int fire_wall : 1;
+    unsigned int earthquake : 1;
+    unsigned int magic_arrow : 1;
+    unsigned int ice_bolt : 1;
+    unsigned int lightning_bolt : 1;
+    unsigned int implosion : 1;
+    unsigned int chain_lightning : 1;
+    unsigned int frost_ring : 1;
+    unsigned int fireball : 1;
+    unsigned int inferno : 1;
+    unsigned int meteor_shower : 1;
+    unsigned int death_ripple : 1;
+    unsigned int destroy_undead : 1;
+    unsigned int shield : 1;
+    unsigned int armageddon : 1;
+    unsigned int air_shield : 1;
+    unsigned int fire_shield : 1;
+    unsigned int prot_from_air : 1;
+    unsigned int prot_from_fire : 1;
+    unsigned int prot_from_water : 1;
+    unsigned int prot_from_earth : 1;
+    unsigned int anti_magic : 1;
+    unsigned int dispel : 1;
+    unsigned int magic_mirror : 1;
+    unsigned int cure : 1;
+    unsigned int resurrection : 1;
+    unsigned int animate_dead : 1;
+    unsigned int sacrifice : 1;
+    unsigned int bless : 1;
+    unsigned int curse : 1;
+    unsigned int bloodlust : 1;
+    unsigned int precision : 1;
+    unsigned int weakness : 1;
+    unsigned int stone_skin : 1;
+    unsigned int disrupting_ray : 1;
+    unsigned int prayer : 1;
+    unsigned int mirth : 1;
+    unsigned int sorrow : 1;
+    unsigned int fortune : 1;
+    unsigned int misfortune : 1;
+    unsigned int haste : 1;
+    unsigned int slow : 1;
+    unsigned int slayer : 1;
+    unsigned int frenzy : 1;
+    unsigned int unused1 : 1;
+    unsigned int counterstrike : 1;
+    unsigned int berserk : 1;
+    unsigned int hypnotize : 1;
+    unsigned int forgetfulness : 1;
+    unsigned int blind : 1;
+    unsigned int teleport : 1;
+    unsigned int remove_obstacle : 1;
+    unsigned int clone : 1;
+    unsigned int fire_elemental : 1;
+    unsigned int earth_elemental : 1;
+    unsigned int water_elemental : 1;
+    unsigned int air_elemental : 1;
+    unsigned int unused2 : 1;
+    unsigned int unused3 : 1;
+};
+
+#pragma pack(pop)
+
+#endif
