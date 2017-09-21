1ab2419de95b515e4d6e91cf5fc57594d37cf2df
RoseKavalier <rosekavalierhc@gmail.com>
2017-09-21 08:24:49 -0400

    Extra h3m constants

---
+++ h3m/h3mlib/h3m_constants/h3m_spells.h
@@ -0,0 +1,89 @@
+#ifndef __H3M_SPELLS_H_DEF__
+#define __H3M_SPELLS_H_DEF__
+
+enum H3M_SPELLS {
+	H3M_SPELLS_Summon_Boat,       /* 0x00 */
+	H3M_SPELLS_Scuttle_Boat,       /* 0x01 */
+	H3M_SPELLS_Visions,       /* 0x02 */
+	H3M_SPELLS_View_Earth,       /* 0x03 */
+	H3M_SPELLS_Disguise,       /* 0x04 */
+	H3M_SPELLS_View_Air,       /* 0x05 */
+	H3M_SPELLS_Fly,       /* 0x06 */
+	H3M_SPELLS_Water_Walk,       /* 0x07 */
+	H3M_SPELLS_Dimension_Door,       /* 0x08 */
+	H3M_SPELLS_Town_Portal,       /* 0x09 */
+	H3M_SPELLS_Quick_Sand,       /* 0x0A */
+	H3M_SPELLS_Land_Mine,       /* 0x0B */
+	H3M_SPELLS_Force_Field,       /* 0x0C */
+	H3M_SPELLS_Fire_Wall,       /* 0x0D */
+	H3M_SPELLS_Earthquake,       /* 0x0E */
+	H3M_SPELLS_Magic_Arrow,       /* 0x0F */
+	H3M_SPELLS_Ice_Bolt,       /* 0x10 */
+	H3M_SPELLS_Lightning_Bolt,       /* 0x11 */
+	H3M_SPELLS_Implosion,       /* 0x12 */
+	H3M_SPELLS_Chain_Lightning,       /* 0x13 */
+	H3M_SPELLS_Frost_Ring,       /* 0x14 */
+	H3M_SPELLS_Fireball,       /* 0x15 */
+	H3M_SPELLS_Inferno,       /* 0x16 */
+	H3M_SPELLS_Meteor_Shower,       /* 0x17 */
+	H3M_SPELLS_Death_Ripple,       /* 0x18 */
+	H3M_SPELLS_Destroy_Undead,       /* 0x19 */
+	H3M_SPELLS_Armageddon,       /* 0x1A */
+	H3M_SPELLS_Shield,       /* 0x1B */
+	H3M_SPELLS_Air_Shield,       /* 0x1C */
+	H3M_SPELLS_Fire_Shield,       /* 0x1D */
+	H3M_SPELLS_Protection_from_Air,       /* 0x1E */
+	H3M_SPELLS_Protection_from_Fire,       /* 0x1F */
+	H3M_SPELLS_Protection_from_Water,       /* 0x20 */
+	H3M_SPELLS_Protection_from_Earth,       /* 0x21 */
+	H3M_SPELLS_Anti_Magic,       /* 0x22 */
+	H3M_SPELLS_Dispel,       /* 0x23 */
+	H3M_SPELLS_Magic_Mirror,       /* 0x24 */
+	H3M_SPELLS_Cure,       /* 0x25 */
+	H3M_SPELLS_Resurrection,       /* 0x26 */
+	H3M_SPELLS_Animate_Dead,       /* 0x27 */
+	H3M_SPELLS_Sacrifice,       /* 0x28 */
+	H3M_SPELLS_Bless,       /* 0x29 */
+	H3M_SPELLS_Curse,       /* 0x2A */
+	H3M_SPELLS_Bloodlust,       /* 0x2B */
+	H3M_SPELLS_Precision,       /* 0x2C */
+	H3M_SPELLS_Weakness,       /* 0x2D */
+	H3M_SPELLS_Stone_Skin,       /* 0x2E */
+	H3M_SPELLS_Disrupting_Ray,       /* 0x2F */
+	H3M_SPELLS_Prayer,       /* 0x30 */
+	H3M_SPELLS_Mirth,       /* 0x31 */
+	H3M_SPELLS_Sorrow,       /* 0x32 */
+	H3M_SPELLS_Fortune,       /* 0x33 */
+	H3M_SPELLS_Misfortune,       /* 0x34 */
+	H3M_SPELLS_Haste,       /* 0x35 */
+	H3M_SPELLS_Slow,       /* 0x36 */
+	H3M_SPELLS_Slayer,       /* 0x37 */
+	H3M_SPELLS_Frenzy,       /* 0x38 */
+	H3M_SPELLS_Titans_Lightning_Bolt,       /* 0x39 */
+	H3M_SPELLS_Counterstrike,       /* 0x3A */
+	H3M_SPELLS_Berserk,       /* 0x3B */
+	H3M_SPELLS_Hypnotize,       /* 0x3C */
+	H3M_SPELLS_Forgetfulness,       /* 0x3D */
+	H3M_SPELLS_Blind,       /* 0x3E */
+	H3M_SPELLS_Teleport,       /* 0x3F */
+	H3M_SPELLS_Remove_Obstacle,       /* 0x40 */
+	H3M_SPELLS_Clone,       /* 0x41 */
+	H3M_SPELLS_Fire_Elemental,       /* 0x42 */
+	H3M_SPELLS_Earth_Elemental,       /* 0x43 */
+	H3M_SPELLS_Water_Elemental,       /* 0x44 */
+	H3M_SPELLS_Air_Elemental,       /* 0x45 */
+	// These abilities are used in creature combat structure
+	H3M_SPELLS_Stone,	/* 0x46 */
+	H3M_SPELLS_Poison,	/* 0x47 */
+	H3M_SPELLS_Bind,	/* 0x48 */
+	H3M_SPELLS_Disease,	/* 0x49 */
+	H3M_SPELLS_Paralyze,/* 0x4A */
+	H3M_SPELLS_Aging,	/* 0x4B */
+	H3M_SPELLS_Death_Cloud,/* 0x4C */
+	H3M_SPELLS_Thunderbolt,/* 0x4D */
+	H3M_SPELLS_Dragonfly_Dispel,/* 0x4E */
+	H3M_SPELLS_Death_Stare,/* 0x4F */
+	H3M_SPELLS_Acid_Breath/* 0x50 */
+};
+
+#endif
